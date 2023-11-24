aa66eef4ff8fd809ac0913e8eb9ef5cc591cb5a6
SajeedAninda <sajaninda@gmail.com>
2023-11-24 19:11:07 +0600

    Created Payment Intent, Posted Data to Database after user Login and Payment Checkout Endpoints

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 let cors = require("cors");
-const app = express()
+const app = express();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// console.log(process.env.STRIPE_SECRET_KEY)
 
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+// app.use(cors());
 app.use(express.json());
 
 
@@ -40,7 +49,86 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             // console.log(result);
             res.send(result);
-          });
+        });
+
+        // POST EMPLOYEE DATA TO USER COLLECTION
+        app.post("/employeeRegister", async (req, res) => {
+            const user = req.body;
+            //   console.log(user);
+            const result = await usersCollection.insertOne(user);
+            // console.log(result);
+            res.send(result);
+        });
+
+        // POST EMPLOYEE DATA WITH SOCIAL LOGIN 
+        app.post("/employeeSocialRegister", async (req, res) => {
+            const userDetails = req.body;
+            let checkEmail = userDetails.email;
+            const existingUser = await usersCollection.findOne({ email: checkEmail });
+
+            if (existingUser) {
+                return res.status(409).json({ error: 'Email already exists' });
+            }
+
+            let result = await usersCollection.insertOne(userDetails);
+            res.send(result);
+        });
+
+        // GET ADMIN DATA BEFORE PAYMENT 
+        app.get("/paymentData/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = {
+                email: email,
+            };
+            const result = await usersCollection.findOne(query);
+            // console.log(result);
+            res.send(result);
+        });
+
+        // STRIPE PAYMENT INTENT 
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+            if (amount <= 0) {
+                return res.send({
+                    clientSecret: "Amount not valid",
+                })
+            }
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+        // UPDATE ADMIN INFO AFTER PAYMENT IS DONE 
+
+        app.patch('/updateAdmin/:email', async (req, res) => {
+            const { email } = req.params;
+            const { role, payableAmount, paymentStatus } = req.body;
+
+            try {
+                const filter = { email: email };
+                const update = {
+                    $set: {
+                        role: role,
+                        payableAmount: payableAmount,
+                        paymentStatus: paymentStatus
+                    }
+                };
+
+                const result = await usersCollection.updateOne(filter, update);
+
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+            }
+        });
 
 
 
